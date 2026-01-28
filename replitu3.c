c3304232e1423a126a4e4538ea9a67e5791a552a
TheJavier37 <235195937+TheJavier37@users.noreply.github.com>
2026-01-28 10:31:25 -0500

    Implement client revenue calculation functionality

--- replitu3.c
+++ replitu3.c
@@ -1,1 +1,61 @@
+#include <stdio.h>
 
+void calcularValorCliente(float *totalRecaudado);
+void calcularValorRecaudado(int numeroClientes);
+
+int main() {
+    int numeroClientes;
+
+    printf("Ingrese el numero de clientes: ");
+    scanf("%i", &numeroClientes);
+  
+    calcularValorRecaudado(numeroClientes);
+
+    return 0;
+}
+
+void calcularValorRecaudado(int numeroClientes) {
+    int i;
+    float totalRecaudado = 0;
+
+    for(i = 1; i <= numeroClientes; i++){
+        calcularValorCliente(&totalRecaudado);
+    }
+
+    printf("El valor total recaudado es: %.2f\n", totalRecaudado);
+}
+
+void calcularValorCliente(float *totalRecaudado) {
+    int tipoConsola;
+    int tiempoJuego;
+    float tarifa;
+    float valorCliente;
+  
+    do {
+        printf("\nIngrese el tipo de consola:\n");
+        printf("1 = PlayStation\n");
+        printf("2 = Xbox\n");
+        printf("3 = Nintendo\n");
+        scanf("%i", &tipoConsola);
+
+        if(tipoConsola < 1 || tipoConsola > 3){
+            printf("Error, ingrese un numero valido\n");
+        }
+
+    } while(tipoConsola < 1 || tipoConsola > 3);
+
+    if(tipoConsola == 1){
+        tarifa = 2.50;
+    } else if(tipoConsola == 2){
+        tarifa = 2.00;
+    } else {
+        tarifa = 1.50;
+    }
+
+    printf("Ingrese el tiempo de juego: ");
+    scanf("%i", &tiempoJuego);
+
+    valorCliente = tarifa * tiempoJuego;
+
+    *totalRecaudado += valorCliente;
+}
